5ea5835997707fcf9ca44145a00ed053b6492e5b
vikhulei <65869915+vikhulei@users.noreply.github.com>
2023-01-28 09:27:58 +0000

    tets

--- src/pages/01_home/01_cost/CostStyles.js
+++ src/pages/01_home/01_cost/CostStyles.js
@@ -2,12 +2,12 @@ import styled from "styled-components"
 import { Wrapper } from "../../../components/ui/GeneralStyles"
 
 const WrapperCost = styled(Wrapper) `
-
+    background-color: red;
 `
 
 const MainImage = styled.img `
     width: 100%;
-    height: 100%;
+    height: 50%;
     object-fit: cover;
 `
 
