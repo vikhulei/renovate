6a0abcc2e45a203d1cadc70dd820aba5170ec600
vikhulei <vikhulei@gmail.com>
2023-03-07 20:16:49 +0000

    footer upperbar drafted

--- src/components/footer/01_upperBar/UpperBar.js
+++ src/components/footer/01_upperBar/UpperBar.js
@@ -1,10 +1,30 @@
-import { Background, Wrapper } from "./UpperBarStyle"
+import { Background, Wrapper, SectionWrapper, IconWrapper, TextWrapper, Text } from "./UpperBarStyle"
 
 const UpperBar = () => {
     return (
         <Background>
             <Wrapper>
-                <h2>Upper Bar</h2>
+                <SectionWrapper>
+                    <IconWrapper></IconWrapper>
+                    <TextWrapper>
+                        <Text>Phone:</Text>
+                        <Text>+149 75 23 222 35</Text>
+                    </TextWrapper>
+                </SectionWrapper>
+                <SectionWrapper>
+                    <IconWrapper></IconWrapper>
+                    <TextWrapper>
+                        <Text>Phone:</Text>
+                        <Text>+149 75 23 222 35</Text>
+                    </TextWrapper>
+                </SectionWrapper>
+                <SectionWrapper>
+                    <IconWrapper></IconWrapper>
+                    <TextWrapper>
+                        <Text>Phone:</Text>
+                        <Text>+149 75 23 222 35</Text>
+                    </TextWrapper>
+                </SectionWrapper>
             </Wrapper>
         </Background>
     )
--- src/components/footer/01_upperBar/UpperBarStyle.js
+++ src/components/footer/01_upperBar/UpperBarStyle.js
@@ -1,12 +1,45 @@
 import styled from "styled-components"
 import { BackgroundGeneral, WrapperGeneral } from "../../ui/GeneralStyles"
+import {Colors} from "../../ui/Colors"
+
+const {orange} = Colors
 
 const Background = styled(BackgroundGeneral) `
-
+    background-color: ${orange};
+    height: 150px;
 `
 
 const Wrapper = styled(WrapperGeneral) `
-
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
 `
 
-export {Background, Wrapper}
+const SectionWrapper = styled.div `
+    width: 30%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    gap: 30px;
+`
+
+const IconWrapper = styled.div `
+    width: 90px;
+    height: 90px;
+    padding: 5px;
+    background-color: white;
+    border-radius: 50px;
+`
+
+const TextWrapper = styled.div `
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    line-height: 2;
+`
+
+const Text = styled.p `
+    color: white;
+`
+
+export {Background, Wrapper, SectionWrapper, IconWrapper, TextWrapper, Text}
